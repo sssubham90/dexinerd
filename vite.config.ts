d3b294bb1868ee837d27d66928a758e729ffa24e
Subham Mohapatra <yoyosssubham@gmail.com>
2025-05-26 04:49:36 +0530

    Removed vite-plugin-dts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import vitePluginSvgr from 'vite-plugin-svgr'
-import dts from 'vite-plugin-dts'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import vitePluginSvgr from "vite-plugin-svgr";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
+    TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
     tailwindcss(),
     react(),
     vitePluginSvgr(),
-    dts({ tsconfigPath: './tsconfig.app.json' }),
   ],
-})
+});
